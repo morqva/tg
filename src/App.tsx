dc600d37e20cdc1c139ebdf2684cf951cbffd0cb
m0rqva <87177473+morqva@users.noreply.github.com>
2025-07-02 19:23:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { TonConnect, WalletConnectionSource } from '@tonconnect/sdk';
 import { useEffect, useState } from 'react';
 
 const connector = new TonConnect({
-  manifestUrl: 'https://hit.com',
+  manifestUrl: 'https://testserverrr.com',
 });
 
 function App() {
